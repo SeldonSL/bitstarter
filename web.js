3856ae839ad5a0972f12fa204d227753a28435b6
Carlos Aravena <carlos.aravena.c@gmail.com>
2013-07-10 16:03:54 +0000

    Testing web.js to read from index.html

--- web.js
+++ web.js
@@ -4,15 +4,15 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var fs = require('fs');
-  fs.readFileSync('index.html', 'null', function (err, data) {
-  if (err) throw err;
+//  fs.readFileSync('index.html', 'null', function (err, data) {
+  //if (err) throw err;
   console.log("aqui");
-  var buffer = new Buffer(data,"utf8");
+  var buffer = new Buffer("kk","utf8");
 
   response.send(buffer.toString("utf8"));
 }
 );
-});
+//});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
